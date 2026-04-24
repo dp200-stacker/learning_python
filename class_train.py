a042d732d1eb140c299b1c27849bdc4a8424d195
PinusKing27 <diogopinheiro300@gmail.com>
2026-04-24 17:33:18 +0100

    Add Garrafa class with example instances

--- class_train.py
+++ class_train.py
@@ -1,3 +1,18 @@
+class Garrafa:
+    def __init__(self, liquido, tamanho):
+        self.liquido = liquido
+        self.tamanho = tamanho
+
+g1 = Garrafa('agua', '250ml')
+g2 = Garrafa('ice tea', '500ml')
+
+print(g1.liquido)
+print(g1.tamanho)
+print(g2.liquido)
+print(g2.tamanho)
+
+############################################
+
 class Relogio:
     def __init__(self, horas, minutos):
         self.horas = horas
@@ -10,3 +25,5 @@ print(r1.horas)
 print(r1.minutos)
 print(r2.horas)
 print(r2.minutos) 
+
+############################################
