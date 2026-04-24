805d6386fca8c741e40b326a8e0800fbb7f34af2
PinusKing27 <diogopinheiro300@gmail.com>
2026-04-24 17:23:07 +0100

    Create class_train.py

---
+++ class_train.py
@@ -0,0 +1,12 @@
+class Relogio:
+    def __init__(self, horas, minutos):
+        self.horas = horas
+        self.minutos = minutos
+
+r1 = Relogio(12 , 3)
+r2 = Relogio(14 , 30)
+
+print(r1.horas)
+print(r1.minutos)
+print(r2.horas)
+print(r2.minutos) 
